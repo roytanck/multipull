5f27413301014ab971f91632b0455366cb4a6d10
Roy Tanck <roy@tanck.nl>
2022-03-14 15:13:03 +0100

    Added the bash script

---
+++ multipull.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+dir=`pwd`
+list=`find -maxdepth 1 -type d`
+
+for i in $list
+do
+	if [ "$i" != "." ]; then
+		echo "--------------"
+		echo "Folder: $i"
+		if [ -d "${i}/.git" ]; then
+			cd $i
+			branch=`git branch --show-current`
+			if [[ "$branch" == "develop" || "$branch" == "master" || "$branch" == "main" ]]; then
+				changes=`git diff --name-only`
+				if [ "$changes" == "" ]; then
+					echo "No uncommitted changes"
+					git pull
+					git fetch --tags
+				else
+					echo "Local branch '$branch' contains uncommitted changes"
+				fi
+			else
+				echo "Repository not on the master/main/develop branch: $branch"
+			fi
+			cd $dir
+		else
+			echo "Not a Git repository"
+		fi
+	fi
+done
